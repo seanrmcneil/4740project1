2fbc99c11427a86dcf3312f3016a7aaebf985536
Sean <srm274@cornell.edu>
2015-09-14 12:02:40 -0400

    this could be much better

--- main.py
+++ main.py
@@ -4,23 +4,20 @@ from tokenizer import tokenize1
 def ngram(Filename):
 	tokens = tokenize1(Filename)
 
-	#works for unigrams
+	#works for unigrams and bigrams
+
 	ngram_dict = {}
-	for i in tokens:
-		if i in ngram_dict:
-			ngram_dict[i] += 1
+	for i in range(len(tokens)-1):
+		if tokens[i] in ngram_dict:
+			ngram_dict[tokens[i]] += 1
 		else:
-			ngram_dict[i] = 1
+			ngram_dict[tokens[i]] = 1
+		if tokens[i] + ' ' + tokens[i + 1] in ngram_dict:
+			ngram_dict[tokens[i] + ' ' + tokens[i + 1]] += 1
+		else:
+			ngram_dict[tokens[i] + ' ' + tokens[i + 1]] = 1
 	print (ngram_dict)
 
-	# works for bigrams
-	ngram_dict2 = {}
-	for i in range(len(tokens)-1):
-		if tokens[i]+' '+tokens[i+1] in ngram_dict2:
-			ngram_dict2[tokens[i]+' '+tokens[i+1]] += 1
-		else:
-			ngram_dict2[tokens[i]+' '+tokens[i+1]] = 1
-	print (ngram_dict2)
 
 
 
