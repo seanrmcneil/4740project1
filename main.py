169c1268e78c6335b3db31d1ef31982560429186
Sean <srm274@cornell.edu>
2015-09-14 20:06:27 -0400

    I made a note

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def ngram(Filename):
 	tokens = tokenize1(Filename)
 
 	#works for unigrams and bigrams
-
 	ngram_dict = {}
 	for i in range(len(tokens)-1):
 		if tokens[i] in ngram_dict:
@@ -18,7 +17,9 @@ def ngram(Filename):
 			ngram_dict[tokens[i] + ' ' + tokens[i + 1]] = 1
 	print (ngram_dict)
 
-
+#create a list with the unigram/bigram and the probability that it appears in a second list at the corresponding index
+#but to do this, we need to know whether each key is a unigram or bigram and the current dictionary won't tell us that
+#possible hacky solution: 2 dictionaries
 
 
 
