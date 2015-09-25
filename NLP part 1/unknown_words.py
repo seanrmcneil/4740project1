2e331bdee6424289975b2fa47de4e40e55cd94b3
Sean <srm274@cornell.edu>
2015-09-25 11:10:29 -0400

    another unkown fix

--- NLP part 1/unknown_words.py
+++ NLP part 1/unknown_words.py
@@ -2,7 +2,7 @@
 
 #practice dictionary
 d = {"one": 1, "oneagain": 1, "three": 3, "four": 4, "eight" : 8, "nine": 9}
-sample= {"hi": {"hey":3}, "banana": {"monkey":2}, "hey":{"ho":3, "this":1}, "monkey":{"eat":6, "loves":1}}
+sample= {"hi": {"hey":3}, "banana": {"monkey":2}, "hey":{"ho":3, "this":1, "is":1}, "monkey":{"eat":6, "loves":1}}
 
 
 
@@ -19,15 +19,18 @@ def unknown_words_uni(unigram_dict):
 
 def unknown_words_bi(bigram_dict):
 	bigram_dict_unk = bigram_dict.copy()
-	bigram_dict_unk["<UNK>"] = 0
+	bigram_dict_unk["<UNK>"] = {"<UNK>": 0}
 
 	for key, dict2 in bigram_dict.iteritems():
+		#bigram_dict_unk[key]["<UNK>"] = 0
 		for name, value in dict2.items():
 			if value == 1:
 				del bigram_dict_unk[key][name]
-				bigram_dict_unk["<UNK>"] = bigram_dict_unk["<UNK>"] + 1
+				#bigram_dict_unk[key]["<UNK>"] =  bigram_dict_unk[key]["<UNK>"] + 1
+				bigram_dict_unk["<UNK>"]["<UNK>"] = bigram_dict_unk["<UNK>"]["<UNK>"] + 1
 
-	return bigram_dict_unk
+
+	print bigram_dict_unk
 
 
 unknown_words_bi(sample)
